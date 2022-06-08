4db19c1aea04623abbae78a2608c541d85534474
Kyle Harrison <kyle90adam@hotmail.com>
2022-06-08 17:37:35 +0100

    adds win and lose conditions

--- src/main.rs
+++ src/main.rs
@@ -76,8 +76,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         // Updates
         player.update(delta);
-        if invaders.update(delta){
+        if invaders.update(delta) {
             audio.play("move");
+        }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
         }
 
         // Draw & render
@@ -87,6 +90,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+
+        // Win or lose?
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop;
+        }
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'gameloop;
+        }
     }
 
     // Cleanup
